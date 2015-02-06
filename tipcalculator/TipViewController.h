87cc917c8d47705a11e6b5e22180b17747b6c621
mkandel <marc.kandel.junk@gmail.com>
2015-02-06 00:20:43 -0800

    wip

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by marc on 2/5/15.
+//  Copyright (c) 2015 marc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ tipcalculator/TipViewController.m
@@ -0,0 +1,39 @@
+//
+//  TipViewController.m
+//  tipcalculator
+//
+//  Created by marc on 2/5/15.
+//  Copyright (c) 2015 marc. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.title = @"Tip Calculator";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
