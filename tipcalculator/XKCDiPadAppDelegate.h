84d025f48d4eb4a05cfb6a47a28638f6106c5dd9
mkandel <marc.kandel.junk@gmail.com>
2015-02-05 23:32:56 -0800

    Initial Commit

---
+++ tipcalculator/XKCDiPadAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XKCDiPadAppDelegate.h
+//  tipcalculator
+//
+//  Created by marc on 2/5/15.
+//  Copyright (c) 2015 marc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XKCDiPadAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by marc on 2/5/15.
+//  Copyright (c) 2015 marc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XKCDiPadAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XKCDiPadAppDelegate class]));
+    }
+}
